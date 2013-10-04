16c15debd4c05cbe1d2b83978240a30c49d02ea6
demoncodemonkey <demoncodemonkey@gmail.com>
2013-10-04 23:29:32 +0100

    testing preprocessors

--- LittleHelpers/Ensure.cs
+++ LittleHelpers/Ensure.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-#if LITTLEHELPERS_NEEDS_EXPLICIT_USING_DIRECTIVES
+#if !LITTLEHELPERS_NEEDS_EXPLICIT_USING_DIRECTIVES
 namespace demoncodemonkey.LittleHelpers
 {
 #endif
